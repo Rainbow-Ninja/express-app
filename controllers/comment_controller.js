2e4dd01d732e44daddb9f5e922f9911ed06618f5
Nandini Nayak <nandini.r.nayak@gmail.com>
2019-10-09 12:53:42 +1100

    denormalised schema

---
+++ controllers/comment_controller.js
@@ -0,0 +1,18 @@
+const BookModel = require('../database/models/book_model');
+const create = async (req, res)=> {
+    let {bookId} = req.params;
+    let {body} = req.body;
+    // find the book
+    let book = await BookModel.findById(bookId)
+        .catch(err => res.status(500).send(err));
+    // add the comments to the book
+    book.comments.push({body});
+    // save the book to the database
+    book.save()
+    res.redirect(`/books/${bookId}`)
+}
+
+module.exports = {
+    create
+}
+
--- routes/book_routes.js
+++ routes/book_routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const BookController = require('../controllers/book_controller');
+const CommentController = require('../controllers/comment_controller');
 
 router.get('/books/new', BookController.make);
 router.post('/books', BookController.create);
-router.get('/books/:id', BookController.show)
+router.get('/books/:id', BookController.show);
+
+router.post('/books/:bookId/comments', CommentController.create );
 
 module.exports = router;
 
